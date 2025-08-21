7edc46560765db595b1012744dafc22fb472d081
Jaroslaw Goszowski <goszowski@gmail.com>
2025-08-21 15:08:26 +0300

    refactor: use self::message() instead of static in GateNotConfigured

--- src/Exceptions/GateNotConfigured.php
+++ src/Exceptions/GateNotConfigured.php
@@ -8,7 +8,7 @@ class GateNotConfigured extends Exception
 {
     public function __construct($message = null, $code = 0, ?Exception $previous = null)
     {
-        parent::__construct($message ?: static::message(), $code, $previous);
+        parent::__construct($message ?: self::message(), $code, $previous);
     }
 
     public static function message(): string
